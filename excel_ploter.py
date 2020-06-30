60389484e2d2653201ebcfe1dfe198fffb43bd0e
copan31 <52942558+copan31@users.noreply.github.com>
2020-06-30 22:21:06 +0900

    バブルチャート実装

--- excel_ploter.py
+++ excel_ploter.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from openpyxl import load_workbook
-from openpyxl.chart import Reference, LineChart, AreaChart, BarChart
+from openpyxl.chart import Reference, Series, LineChart, AreaChart, BarChart, BubbleChart
 import os
 
 class ExcelPloter:
     def __init__(self, csvfile):
         self._df = pd.read_csv(csvfile)
-        self._df["DateTime"] = pd.to_datetime(self._df["公表_年月日"])
-        self._df = self._df.set_index("DateTime")
+        # self._df["DateTime"] = pd.to_datetime(self._df["公表_年月日"])
+        # self._df = self._df.set_index("DateTime")
         self._file = "test.xlsx"
         print(self._df)
 
@@ -29,6 +29,26 @@ class ExcelPloter:
         chart.set_categories(Reference(sheet, min_col = 1, max_col = 1, min_row = 2, max_row = sheet.max_row)) # ラベル
         sheet.add_chart(chart, "A" + str(position))
 
+    def _plot_bubble(self, sheet, title, xlabel, ylabel):
+
+        chart = BubbleChart()
+        chart.style = 18
+
+        # plot on the sheet
+        chart.title = title
+        chart.x_axis.title = xlabel
+        chart.y_axis.title = ylabel
+        chart.y_axis.scaling.min = 0
+        chart.legend.position = 'r' # 凡例は右に設定
+
+        for row in range(2, sheet.max_row + 1):
+            xvalues = Reference(sheet, min_col = 3, min_row = row)
+            yvalues = Reference(sheet, min_col = 2, min_row = row)
+            size    = Reference(sheet, min_col = 4, min_row = row)
+            chart.series.append(Series(values = yvalues, xvalues = xvalues, zvalues = size, title = sheet.cell(row, 1).value))
+
+        sheet.add_chart(chart, "A1")
+
     def reload(self):
         self._df = pd.read_csv(csvfile)
 
@@ -49,6 +69,22 @@ class ExcelPloter:
             # plot line chart
             chart = LineChart()            
             self._plot(writer.book[sheetname], chart, title = "線グラフ", xlabel = "日付", ylabel = "回数")
+    
+    def plot_bubble(self, sheetname):
+        # 必要な値と項目を残した時系列データを作成
+        df = self._df[["name", "age", "point"]]
+        df["x"] = df["age"] * 2
+        df = df.set_index("name")
+
+        with pd.ExcelWriter(self._file) as writer:
+            # excelファイルがあるなら追加書きする
+            if os.path.isfile(self._file):
+                writer.book = load_workbook(self._file)
+            # write the df on excel at once
+            df.to_excel(writer, sheet_name = sheetname, index = True)
+
+            # plot line chart      
+            self._plot_bubble(writer.book[sheetname], title = "バブル", xlabel = "日付", ylabel = "回数")
 
     def plot_area(self, sheetname, column, value):
         # 必要な値と項目を残した時系列データを作成
@@ -122,3 +158,5 @@ if __name__ == "__main__":
     csvfile = "130001_tokyo_covid19_patients(1).csv"
     csvfile = "sample_pandas_normal.csv"
     a = ExcelPloter(csvfile)
+
+    a.plot_bubble("a")
